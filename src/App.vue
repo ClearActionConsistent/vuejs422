9cb3cb01e356c4203b6d0d13e7b85b38698311a3
Trung Nguyen <trung.nguyenchi@brightstart.com>
2020-02-20 16:34:51 +0700

    [for loop object] [v-for] [v-bind:key] [component data].

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,64 @@
 <template>
   <div id="app">
-    <HelloWorldComponent msg="getting start with vue"/>
+    <ul>
+		<li v-for="todo in todos" v-bind:key="todo">
+			{{todo.title}}
+		</li>
+	</ul>
   </div>
 </template>
 
 <script>
-import HelloWorldComponent from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorldComponent
+    
+  },
+  data(){
+	return {
+		todos: [
+			{
+				id: 1,
+				title: 'Todo One',
+				completed: false
+			},
+			{
+				id: 2,
+				title: 'Todo Two',
+				completed: true
+			},
+			{
+				id: 3,
+				title: 'Todo Three',
+				completed: false
+			}
+		]
+	}
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+* {
+	box-sizing: border-box;
+	margin: 0;
+	padding: 0;
+  }
+  body {
+	font-family: Arial, Helvetica, sans-serif;
+	line-height: 1.4;
+  }
+  .btn {
+	display: inline-block;
+	border: none;
+	background: #555;
+	color: #fff;
+	padding: 7px 20px;
+	cursor: pointer;
+  }
+  .btn:hover {
+	background: #666;
+  }
 </style>
