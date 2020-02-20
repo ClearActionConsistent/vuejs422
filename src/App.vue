8a30f044af8a88240d8af2ccab0b2193f2bda0f3
Trung Nguyen <trung.nguyenchi@brightstart.com>
2020-02-20 16:17:30 +0700

    just remove unneccessary code and edit some stuff to know how the app get started

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorldComponent msg="getting start with vue"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorldComponent from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorldComponent
   }
 }
 </script>
